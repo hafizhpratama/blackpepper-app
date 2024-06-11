0011e03616b5d6cd8e9fee85715c97770630a7e2
Hafizh Pratama <hafizhpratama99@gmail.com>
2024-06-11 18:12:16 +0800

    fix: update layout.tsx

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,12 +1,15 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
-import { redirect } from "next/navigation";
 import Header from "@/components/admin/header";
-import { LayoutProps } from "@/types/attribute";
 import Footer from "@/components/footer";
+import { redirect } from "next/navigation";
 
-const Layout: React.FC<LayoutProps> = async ({ children }) => {
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const session = await getServerSession(authOptions);
 
   if (!session) {
@@ -16,12 +19,10 @@ const Layout: React.FC<LayoutProps> = async ({ children }) => {
   return (
     <div className="flex min-h-screen w-full flex-col">
       <Header />
-      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+      <main className="flex min-h-[calc(100vh - _theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
         {children}
       </main>
       <Footer />
     </div>
   );
-};
-
-export default Layout;
+}
